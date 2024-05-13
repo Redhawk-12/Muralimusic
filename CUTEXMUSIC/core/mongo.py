b2c71155a478dcec46fa7879961ee83d8b03044a
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-13 13:52:07 +0530

    Update mongo.py

--- CUTEXMUSIC/core/mongo.py
+++ CUTEXMUSIC/core/mongo.py
@@ -1,27 +1,26 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from async_pymongo import AsyncMongoClient
 from pyrogram import Client
 from typing import Dict, Union
 import config
 from ..logging import LOGGER
 from config import *
 
+
 TEMP_MONGODB = "mongodb+srv://CutexMusicdatabase:Cute937@cutedatabase.ubypaox.mongodb.net/?retryWrites=true&w=majority&appName=CuteDatabase"
 
-
-_mongo_async_ = AsyncIOMotorClient(config.MONGO_DB_URI if config.MONGO_DB_URI else TEMP_MONGODB)
-pymongodb = _mongo_async_.CUTEXMUSIC if config.MONGO_DB_URI else _mongo_async_[info.username]
-
-#### next
+_mongo_async_ = AsyncMongoClient(config.MONGO_DB_URI if config.MONGO_DB_URI else TEMP_MONGODB)
 
 
+pymongodb = _mongo_async_.get_database('CUTEXMUSIC' if config.MONGO_DB_URI else info.username)
 
-mongo = AsyncIOMotorClient(MURALI_DB)
-db = mongo.MURALIBOTDATABSE
-coupledb = db.couple
-afkdb = db.afk
-nightmodedb = db.nightmode
-notesdb = db.notes
-filtersdb = db.filters
+
+mongo = AsyncMongoClient(config.MURALI_DB)
+db = mongo.get_database('MURALIBOTDATABASE')
+coupledb = db.get_collection('couple')
+afkdb = db.get_collection('afk')
+nightmodedb = db.get_collection('nightmode')
+notesdb = db.get_collection('notes')
+filtersdb = db.get_collection('filters')
 
 
 async def _get_lovers(cid: int):
