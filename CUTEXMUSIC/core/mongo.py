2fbbfdb7cd271370e9759fa54db9f22ee015de60
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-13 14:39:14 +0530

    Update mongo.py

--- CUTEXMUSIC/core/mongo.py
+++ CUTEXMUSIC/core/mongo.py
@@ -1,14 +1,12 @@
-from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
 from pyrogram import Client
-from typing import Dict, Union
 import config
 from ..logging import LOGGER
 from config import *
 
 TEMP_MONGODB = "mongodb+srv://CutexMusicdatabase:Cute937@cutedatabase.ubypaox.mongodb.net/?retryWrites=true&w=majority&appName=CuteDatabase"
 
-
+# Create a MongoClient object
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning("No MONGO DB URL found.")
     temp_client = Client(
@@ -21,27 +19,20 @@ if config.MONGO_DB_URI is None:
     info = temp_client.get_me()
     username = info.username
     temp_client.stop()
-    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
-    _mongo_sync_ = MongoClient(TEMP_MONGODB)
-    mongodb = _mongo_async_[username]
-    pymongodb = _mongo_sync_[username]
+    mongodb = MongoClient(TEMP_MONGODB)[username]
 else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.CUTEXMUSIC
-    pymongodb = _mongo_sync_.CUTEXMUSIC
+    mongodb = MongoClient(config.MONGO_DB_URI).CUTEXMUSIC
 
-#### next
-
-
-
-mongo = _mongo_client_(MURALI_DB)
-db = mongo.MURALIBOTDATABSE
+# Define other databases and collections
+mongo = MongoClient(config.MURALI_DB)
+db = mongo.MURALIBOTDATABASE
 coupledb = db.couple
 afkdb = db.afk
 nightmodedb = db.nightmode
 notesdb = db.notes
 filtersdb = db.filters
+
+
 
 
 async def _get_lovers(cid: int):
