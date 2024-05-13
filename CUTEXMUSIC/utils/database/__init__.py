0c999947436ce425afac2aa2edfb790c1f60b53f
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-13 16:57:29 +0530

    Update __init__.py

--- CUTEXMUSIC/utils/database/__init__.py
+++ CUTEXMUSIC/utils/database/__init__.py
@@ -2,13 +2,8 @@ from .assistantdatabase import *
 from .memorydatabase import *
 from .mongodatabase import *
 from .randomthumb import *
-from pymongo import MongoClient
 from config import *
 
-DBNAME = "CUTIEXMUSICBOT"
 
-# Connect to MongoDB
-mongo = MongoClient(MURALI_DB)
-dbname = mongo[DBNAME]
 
 
