11ae5b2e771606762809d3a0a92526f173dc27bd
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-14 20:27:56 +0530

    Update ZNewWel.py

--- CUTEXMUSIC/plugins/MyTools/ZNewWel.py
+++ CUTEXMUSIC/plugins/MyTools/ZNewWel.py
@@ -59,10 +59,10 @@ def welcomepic(pic, user, chat, id, uname, Thumbnail):
     draw = ImageDraw.Draw(background)
    # font = ImageFont.truetype('assets/font.ttf', size=160)
     font2 = ImageFont.truetype('assets/font.ttf', size=50)
-    draw.text((48, 40), f"CUTEXMUSIC", fill=(255, 255, 255), font=font2)
+    draw.text((48, 37), f"CUTEXMUSIC", fill=(255, 255, 255), font=font2)
    # draw.text((1680, 1120), f'ID: {id}', fill=(255, 255, 255), font=font)
   #  draw.text((1680, 1380), f"USERNAME : {uname}", fill=(255,255,255),font=font)
-    pfp_position = (120, 400)  
+    pfp_position = (150, 350)  
     background.paste(pfp, pfp_position, pfp)  
     background.save(
         f"downloads/welcome#{id}.png"
