4150167a3de6b3be30dc2d3a7eb7eb8fa079b1b7
𝒁𝒆𝒓𝒐 𝐗 𝑪𝒐𝒅𝒆𝒓𝒁 🦅࿐ <113998368+ZeroXCoderZ@users.noreply.github.com>
2024-05-21 14:27:56 +0530

    Update errors.py

--- CUTEXMUSIC/utils/errors.py
+++ CUTEXMUSIC/utils/errors.py
@@ -26,7 +26,6 @@ def split_limits(text):
     return result
 
 
-
 def capture_err(func):
     @wraps(func)
     async def capture(client, message, *args, **kwargs):
@@ -36,7 +35,12 @@ def capture_err(func):
             await app.leave_chat(message.chat.id)
             return
         except Exception as err:
-            errors = traceback.format_exc()
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type,
+                value=exc_obj,
+                tb=exc_tb,
+            )
             error_feedback = split_limits(
                 "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
                     0 if not message.from_user else message.from_user.id,
