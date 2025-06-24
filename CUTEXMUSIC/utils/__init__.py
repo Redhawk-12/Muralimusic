f4bce488130fa4f59337209e10fb8e0c017b8224
Red Häwk <128895420+Redhawk-12@users.noreply.github.com>
2025-06-25 02:07:47 +0530

    Update __init__.py

--- CUTEXMUSIC/utils/__init__.py
+++ CUTEXMUSIC/utils/__init__.py
@@ -1,35 +1,7 @@
-from .channelplay import register_channel_players
-from .database import init_db, close_db
-from .decorators import admin_only, handle_errors
-from .formatters import (
-    format_duration,
-    time_to_seconds,
-    bytes_to_human
-)
-from .inline import setup_inline_handlers
-from .sys import (
-    restart_bot,
-    update_bot,
-    get_logs
-)
-
-__all__ = [
-    'register_channel_players',
-    'init_db',
-    'close_db',
-    'admin_only',
-    'handle_errors',
-    'format_duration',
-    'time_to_seconds',
-    'bytes_to_human',
-    'setup_inline_handlers',
-    'restart_bot',
-    'update_bot',
-    'get_logs',
-]
-
-def setup_utils(app):
-    """Initialize all utility modules"""
-    init_db()
-    setup_inline_handlers(app)
-    # any other setup
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .formatters import *
+from .inline import *
+from .pastebin import *
+from .sys import *
